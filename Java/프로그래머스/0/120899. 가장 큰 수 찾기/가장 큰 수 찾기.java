ca9304b3e48c437368028a9f2dcec9b5e5823275
kiri <zlfl15@naver.com>
2024-09-10 11:16:02 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int max = array[0];
+        for(int i=1;i<array.length;i++){
+            if(array[i]>max){
+                max = array[i];
+                answer[0] = max;
+                answer[1] = i;
+            }
+        }
+        return answer;
+    }
+}
