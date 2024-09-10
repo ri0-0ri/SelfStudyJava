f85f0166d9faa1adb3f2da13b7286f10e10aee8d
kiri <zlfl15@naver.com>
2024-09-10 11:12:52 +0900

    [level 0] Title: 대문자와 소문자, Time: 18.57 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i=0; i<my_string.length() ;i++){
+            if(my_string.charAt(i)<97){
+                answer += (char)(my_string.charAt(i)+32);
+            }
+            else{
+                answer += (char)(my_string.charAt(i)-32);
+            }
+        }
+        return answer;
+    }
+}
