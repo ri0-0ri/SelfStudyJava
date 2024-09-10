69fac20382a157ea182ea0ae81fb2e7e30b191ad
kiri <zlfl15@naver.com>
2024-09-10 13:49:37 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.06 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+        for(int i=0; i<my_string.length() ;i++){
+            if(my_string.substring(0,i).equals(is_prefix)){
+                answer = 1;
+            }
+        }
+        return answer;
+    }
+}
